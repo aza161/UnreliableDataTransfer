eb2b8ae3cbe1719f5f093c5d55f7f2ee596d87bf
ahmad <aza29@mail.aub.edu>
2025-04-29 01:22:43 +0300

    removed file

--- src/client/RUDPSource.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-import java.io.IOException;
-import java.net.InetAddress;
-
-public interface RUDPSource {
-    void sendFile(String filePath, InetAddress destAddress, int destPort, InetAddress channelAddress, int channelPort) throws IOException;
-}
